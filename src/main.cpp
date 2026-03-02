b68ef58c703cdf8a00500590fd7abe402db91f68
lzx <yangzhixinggez@gmail.com>
2026-03-02 16:59:57 +0800

    chore: init project structure for WebServer (Day 0)

---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    std::cout << ">>> High Performance Web Server Initialized (MVP 2)" << std::endl;
+    std::cout << ">>> Ready to link AsyncLogger later..." << std::endl;
+    return 0;
+}
